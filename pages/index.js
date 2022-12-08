a3ae27b42312c970382d2e41443e5c7bb2e4f0b8
fedefiles <intimarca@gmail.com>
2022-12-08 09:18:42 -0300

    button and input change

--- pages/index.js
+++ pages/index.js
@@ -37,13 +37,13 @@ export default function Home() {
       <div className='relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 px-4 text-white z-10'>
        <form >
         <div>
-        <input onChange={(e) => setCity(e.target.value)} type="text" placeholder='Ingresa tu Ciudad'></input>
+        <input onChange={(e) => setCity(e.target.value)} type="text" placeholder='Ingresa tu Ciudad' class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
         
         </div>
 
        </form>
         
-       <button onClick={fetchWeather}>
+       <button onClick={fetchWeather} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
               Buscar Ciudad
             </button>
       </div>
